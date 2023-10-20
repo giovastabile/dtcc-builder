d2e1ccc275a6371fdd457fa1594c0b9ea37642ef
giovastabile <giovanni.stabile87@gmail.com>
2023-10-20 16:53:49 +0200

    minimal change

---
+++ tests/data/MinimalCase/test_dtcc_build.py
@@ -0,0 +1,54 @@
+# Copyright (C) 2023 Anders Logg
+# Licensed under the MIT License
+#
+# This demo illustrates how to build a city model from raw data,
+# essentially equivalent to running the dtcc-build command-line
+# utility, but with more control over the process.
+# %%
+from dtcc import *
+from pathlib import Path
+
+# Set data paths
+data_directory = Path(".")
+buildings_path = data_directory / "PropertyMap.shp"
+pointcloud_path = data_directory / "pointcloud.las"
+
+# Set parameters
+p = parameters.default()
+print(p)
+p["auto_domain"] = True
+# p["debug"] = True
+# %%
+# Calculate bounds
+origin, bounds = calculate_bounds(buildings_path, pointcloud_path, p)
+
+# Load data from file
+city = load_city(buildings_path, bounds=bounds)
+pointcloud = load_pointcloud(pointcloud_path, bounds=bounds)
+# %%
+
+# Build city model
+city = build_city(city, pointcloud, bounds, p)
+# %%
+
+# Build ground mesh and building mesh (surface meshes)
+ground_mesh, building_mesh = build_mesh(city, p)
+# %%
+
+# Build city mesh and volume mesh (tetrahedral mesh)
+volume_mesh, volume_mesh_boundary = build_volume_mesh(city, p)
+# %%
+
+# Save data to file
+city.save(data_directory / "city.pb")
+ground_mesh.save(data_directory / "ground_mesh.pb")
+building_mesh.save(data_directory / "building_mesh.pb")
+volume_mesh.save(data_directory / "volume_mesh.pb")
+volume_mesh_boundary.save(data_directory / "volume_mesh_boundary.pb")
+
+# %% View data
+city.view()
+# %%
+pointcloud.view()
+
+# %%
